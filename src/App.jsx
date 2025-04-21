b120886e242b3652e3d3309557bb2dd0887cc0ca
MuhammadTanvirHasan98 <muhammadtanvirhasan98@gmail.com>
2025-04-22 00:31:53 +0600

    Initial Setup done

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <header>
+         {/* Navabr will be placed here */}
+      </header>
+
+      <main>
+        {/* Main all components will be placed here */}
+      </main>
+
+      <footer>
+        {/* Footer will be placed here */}
+        
+      </footer>
+    </>
+  );
+}
+
+export default App;
